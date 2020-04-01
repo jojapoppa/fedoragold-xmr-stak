3766b6e98a2b7072e0591c32d5ec5d172f23aa84
jojapoppa <jojapoppa@protonmail.com>
2020-03-31 21:58:45 -0700

    added full params needed to run without any prompts...

--- runfedxmrstak.sh
+++ runfedxmrstak.sh
@@ -1,1 +1,1 @@
-./xmr-stak --url 173.249.27.160:3333 --pass fedoragold_wallet --user NJrpNXUhLVyQD7cjmQGu5JiZWR1md5z11SLtyZ3wyRgLFfwS8vXv1iRDK4KqiEYUnmTPL857XENijCh3nbXdhXheJYhZu2Q
+./xmr-stak --url 173.249.27.160:3333 --pass fedoragold_wallet --httpd 0 --currency fedoragold --rigid 0 --user NJrpNXUhLVyQD7cjmQGu5JiZWR1md5z11SLtyZ3wyRgLFfwS8vXv1iRDK4KqiEYUnmTPL857XENijCh3nbXdhXheJYhZu2Q
