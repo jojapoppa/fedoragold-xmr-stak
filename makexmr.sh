37ff58a9a81e6b7c8c3101ff2c040c4ad41c97ff
Joja Poppa <jojapoppa@tradebots.workgroup>
2020-06-12 15:43:23 -0700

    build update...

--- makexmr.sh
+++ makexmr.sh
@@ -5,8 +5,13 @@ from build...
 
 # -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl  (or some local folder...)
 # May need this for Catalina support!!
+
+
 cmake -DXMR-STAK_COMPILE=generic -DOpenCL_ENABLE=OFF -DCUDA_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF -DOpenSSL_ENABLE=OFF -DCMAKE_BUILD_TYPE=Release ..
 make
+
+
+
 
 #sudo apt install nvidia-cuda-toolkit --fix-missing
 #sudo apt-get install libmicrohttpd-dev
